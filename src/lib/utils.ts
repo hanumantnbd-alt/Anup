766f9ee514165e23ffd2dc683ece908ad7a4bc73
hanumantnbd-alt <hanumantnbd@gmail.com>
2026-07-12 08:00:21 +0530

    Create utility functions in utils.ts
    Added utility functions for class name merging and date formatting.

---
+++ src/lib/utils.ts
@@ -0,0 +1,14 @@
+import { type ClassValue, clsx } from 'clsx';
+import { PureComponent } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(date: Date): string {
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'short',
+    year: 'numeric',
+  }).format(new Date(date));
+}
